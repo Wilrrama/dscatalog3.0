379b5d6816e687a4bca1b855820e4031dc492fab
wilrrama@hotmail.com <wilrrama@hotmail.com>
2021-08-27 19:32:19 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -18,9 +22,14 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 	
-	public Category () {
-		
-	}
+	@Column (columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column (columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
+		public Category () {
+		}
 
 	public Category(Long id, String name) {
 		this.id = id;
@@ -41,6 +50,24 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdateAt() {
+		return updatedAt;
 	}
 
 	@Override
@@ -67,9 +94,5 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
 	
 }
